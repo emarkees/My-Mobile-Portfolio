dafe098302474de9b5b9e3cf73249c73117b21aa
emarkees <rphillipse@gmail.com>
2023-01-05 16:59:09 -0600

    script.js link added to the html file

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector("#hamburger");
 
-const menu = document.querySelector("#menu");
+        const menu = document.querySelector("#menu");
         
-const body = document.querySelector("body");
+        const body = document.querySelector("body");
         
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-      menu.classList.toggle("open");
-        body.classList.toggle("fixed");
+        hamburger.addEventListener("click", () => {
+          hamburger.classList.toggle("active");
+          menu.classList.toggle("open");
+          body.classList.toggle("fixed");
           body.classList.toggle("blur");
-});
+        });
