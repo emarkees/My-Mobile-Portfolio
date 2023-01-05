bd6854d1e55ef0fc20ca4000bc0ecc1871024529
emarkees <rphillipse@gmail.com>
2023-01-05 16:51:19 -0600

    mobile menu hamburger script.js updated

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-	$('#amburger').click(function(){
-		$(this).toggleClass('open');
-	});
+const hamburger = document.querySelector("#hamburger");
+
+const menu = document.querySelector("#menu");
+        
+const body = document.querySelector("body");
+        
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+      menu.classList.toggle("open");
+        body.classList.toggle("fixed");
+          body.classList.toggle("blur");
 });
