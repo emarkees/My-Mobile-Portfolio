3bb64241d25befde93ca8a730ccfd795f061de5e
emarkees <rphillipse@gmail.com>
2023-01-06 21:57:22 -0600

    Added modal work script.js

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ const hamburger = document.querySelector("#hamburger");
           body.classList.toggle("fixed");
           body.classList.toggle("blur");
         });
+
+
+const theModal = document.querySelector('#myModal');
+const closeModal = document.querySelector('modal_hamburger');
+const worksSection = document.querySelector('.work-container');
+
+const cardWork = [
+
+  {
+    id: "#one",  
+    title: "Tonic",
+    Cartegories: ['CANOPY', 'Back End Dev', '2015'],
+    img: "./Scr/images/avocado.png",
+    shortDescription:"A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ['html', 'css', 'javaScript'],
+    linkToLive: 'https://github.com/emarkees/My-Mobile-Portfolio',
+    linkToSource: 'https://emarkees.github.io/My-Mobile-Portfolio/',
+  },
+  {
+    id: "#one",  
+    title: "Tonic",
+    Cartegories: ['CANOPY', 'BACK END DEV', '2015'],
+    img: "./Scr/images/avocao.png",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technologies: ['html', 'css', 'javaScript'],
+    linkToLive: 'https://alexisjw.github.io/set-up-myFirst-mobile/',
+    linkToSource: 'https://github.com/AlexisJW/set-up-myFirst-mobile',
+  },
+]
